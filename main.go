23d9f510630f2637847c2ffe39b9e6b6afd1585d
Andreas Zeissner <andi@wundergraph.com>
2024-09-18 09:09:56 +0200

    chore: init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigName("config") 
+	viper.SetConfigType("yaml") 
+	viper.AddConfigPath(".") 
+
+	err := viper.ReadInConfig() 
+	if err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	fmt.Println("Value:", viper.GetString("key")) 
+}
